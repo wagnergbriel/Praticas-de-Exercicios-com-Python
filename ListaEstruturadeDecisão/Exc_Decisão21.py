6208b8ce43d2d8df10db102d5f115d829e4635fc
wagnergbriel <wagnergabriel220596@gmail.com>
2021-04-21 17:36:10 -0300

    Alteraçao no codigo 21 da estrutura de decisao

--- ListaEstruturadeDecisão/Exc_Decisão21.py
+++ ListaEstruturadeDecisão/Exc_Decisão21.py
@@ -14,5 +14,34 @@ Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 
 uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
 '''
 
-def saque(dinheiro:int):
-    print("----------")
+def saque(dinheiro):
+    saque = dinheiro
+    
+    if dinheiro >= 10 and dinheiro <= 600:
+        #Calculando a quantidade de notas
+        notasde100 = int(dinheiro / 100)
+        dinheiro = dinheiro % 100
+
+        notasde50 = int(dinheiro / 50)
+        dinheiro = dinheiro % 50
+
+        notasde10 = int(dinheiro / 10)
+        dinheiro = dinheiro % 10
+
+        notasde5 = int(dinheiro / 5)
+        dinheiro = notasde5 % 5
+
+        notasde1 = dinheiro
+
+        print(f'Saque de {saque}, você receberá notas:\n\
+                                    {notasde100} de 100,\n\
+                                    {notasde50} de 50,\n\
+                                    {notasde10} de 10,\n\
+                                    {notasde5} de 5,\n\
+                                    {notasde1} de 1.')
+
+    else: print('O valor do saque deve está entre 10 e 600 reais.')
+
+
+if __name__ == '__main__':
+    saque(389)
