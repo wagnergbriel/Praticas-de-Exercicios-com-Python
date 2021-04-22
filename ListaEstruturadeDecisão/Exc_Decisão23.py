e4b75ca2897524e507ba842d81205234b6b7f7bd
wagnergbriel <wagnergabriel220596@gmail.com>
2021-04-22 17:11:39 -0300

    Alteraçao no codigo 23 da estrutura de decisao

---
+++ ListaEstruturadeDecisão/Exc_Decisão23.py
@@ -0,0 +1,14 @@
+''' Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
+Dica: utilize uma função de arredondamento.'''
+
+def verificar_conjunto_numerico(numero):
+    if isinstance(numero, int):
+        return f'{numero} é inteiro.'
+    
+    elif isinstance(numero, float):
+        return f'{numero} é decimal.'
+        
+    if not type(numero) is int or float: 
+        raise TypeError('A variável inserida não é int ou float.')
+
+print(verificar_conjunto_numerico(1.56))
