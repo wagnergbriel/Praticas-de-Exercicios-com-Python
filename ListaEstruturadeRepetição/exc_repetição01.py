c88627d9c1471cc5d11992f06ad22732dbb19819
wagnergbriel <wagnergabriel220596@gmail.com>
2021-05-13 20:46:15 -0300

    Conclusao do Exercicio de Est de Rpeticao Num 01

--- ListaEstruturadeRepetição/exc_repetição01.py
+++ ListaEstruturadeRepetição/exc_repetição01.py
@@ -1,14 +1,13 @@
 '''
-Faça um programa que peça uma nota, entre zero e dez. 
+Faça um programa que peça uma nota, entre zero e dez.
 Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe o valor
 válido.
 '''
-bool = True
-while(bool):
-    nota = int(input('Digite uma nota entre 0 e 10.\n'))
-    
+BOOL = True
+while BOOL:
+    nota = int(input('Digite uma nota entre 0 e 10.\n'))    
     if nota == 10:
         print('Você acertou o valor da nota')
-        bool = False
+        BOOL = False
     else:
         print('Você errou o valor da nota.')
