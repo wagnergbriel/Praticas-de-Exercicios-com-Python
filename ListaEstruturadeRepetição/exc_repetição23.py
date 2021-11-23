e73b6b0780316c32d4ac6f2f5ab56a42c18d63c6
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-23 19:49:11 -0300

    feat: adicionar exercicio de repeticao numero 23

--- ListaEstruturadeRepetição/exc_repetição23.py
+++ ListaEstruturadeRepetição/exc_repetição23.py
@@ -1,3 +1,11 @@
 ''' Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário.
 O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos.
 Serão avaliados o funcionamento, o estilo e o número de testes (divisões) executados.'''
+numeroprimo = int(input("Digite o número para verificar se é primo ou não:\n"))
+divisor = 0
+
+for i in range(1, numeroprimo):
+    if i % 2 == 1 and i != 2:
+    #divisor += 1
+    #divisor += 1
+        print(f"{i} é um número primo", end=" | ")
