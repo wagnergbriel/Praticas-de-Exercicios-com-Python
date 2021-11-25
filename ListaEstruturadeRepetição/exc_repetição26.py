f62d7cac3afad129746d2084a7e669b1afdd32ed
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:36:45 -0300

    refactor: reajustar nome das variaveis do exc_repeticao26

--- ListaEstruturadeRepetição/exc_repetição26.py
+++ ListaEstruturadeRepetição/exc_repetição26.py
@@ -1,25 +1,27 @@
 """Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. 
 Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato. """
-quantidadeeleitores = int(input("Digite a quantidade de eleitores para a eleição:\n"))
-somadevotoscandidato1 = 0
-somadevotoscandidato2 = 0
-somadevotoscandidato3 = 0
+quantidade_de_eleitores = int(
+    input("Digite a quantidade de eleitores para a eleição:\n")
+)
+soma_de_votos_candidato1 = 0
+soma_de_votos_candidato2 = 0
+soma_de_votos_candidato3 = 0
 
-for eleitor in range(1, quantidadeeleitores + 1):
+for eleitor in range(1, quantidade_de_eleitores + 1):
     voto = int(
         input(
             "Em quem deseja votar?\nCandidatos:\n1 - Dória\n2 - Bolsonaro\n3 - Ciro\n"
         )
     )
     if voto == 1:
-        somadevotoscandidato1 += 1
+        soma_de_votos_candidato1 += 1
     elif voto == 2:
-        somadevotoscandidato2 += 1
+        soma_de_votos_candidato2 += 1
     elif voto == 3:
-        somadevotoscandidato3 += 1
+        soma_de_votos_candidato3 += 1
     else:
         print("Não existe essa opção de voto")
 
 print(
-    f"Apuração dos votos:\nDória: {somadevotoscandidato1} votos\nBolsonaro: {somadevotoscandidato2} votos\nCiro: {somadevotoscandidato3} votos"
+    f"Apuração dos votos:\nDória: {soma_de_votos_candidato1} votos\nBolsonaro: {soma_de_votos_candidato2} votos\nCiro: {soma_de_votos_candidato3} votos"
 )
