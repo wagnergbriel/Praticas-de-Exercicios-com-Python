504ad2749775dcf7d586eb0baf1e17d2ce00feb1
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:41:00 -0300

    refactor: reajustar nome das variaveis do exc_repeticao25

--- ListaEstruturadeRepetição/exc_repetição25.py
+++ ListaEstruturadeRepetição/exc_repetição25.py
@@ -1,18 +1,18 @@
 """ Faça um programa que peça para n pessoas a sua idade, ao final o programa devera verificar 
 se a média de idade da turma varia entre 0 e 25,26 e 60 e maior que 60; e então, dizer se a turma é 
 jovem, adulta ou idosa, conforme a média calculada."""
-quantidadealunos = int(input("Informe a quantidade de alunos na turma:\n"))
-somadaidade = 0
+quantidade_de_alunos = int(input("Informe a quantidade de alunos na turma:\n"))
+soma_da_idade = 0
 
-for i in range(1, quantidadealunos + 1):
-    idadedoaluno = int(input(f"\nInforme a idade do {i}° aluno:\n"))
-    somadaidade += idadedoaluno
+for i in range(1, quantidade_de_alunos + 1):
+    idade_do_aluno = int(input(f"\nInforme a idade do {i}° aluno:\n"))
+    soma_da_idade += idade_do_aluno
 
-mediadasidades = somadaidade / quantidadealunos
+media_das_idades = soma_da_idade / quantidade_de_alunos
 
-if mediadasidades > 0 and mediadasidades <= 25:
-    print(f"\nA média das idades jovem. Média: {mediadasidades:0.1f}")
-elif mediadasidades > 25 and mediadasidades <= 60:
-    print(f"\nA média das idades adulta. Média: {mediadasidades:0.1f}")
-elif mediadasidades > 60:
-    print(f"\nA média das idades idosa. Média: {mediadasidades:0.1f}")
+if media_das_idades > 0 and media_das_idades <= 25:
+    print(f"\nA média das idades jovem. Média: {media_das_idades:0.1f}")
+elif media_das_idades > 25 and media_das_idades <= 60:
+    print(f"\nA média das idades adulta. Média: {media_das_idades:0.1f}")
+elif media_das_idades > 60:
+    print(f"\nA média das idades idosa. Média: {media_das_idades:0.1f}")
