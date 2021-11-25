70f0063854dc594405688e0a609ac42ab8ddc365
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:33:47 -0300

    refactor: reajustar nome das variaveis do exc_repeticao27

--- ListaEstruturadeRepetição/exc_repetição27.py
+++ ListaEstruturadeRepetição/exc_repetição27.py
@@ -1,19 +1,19 @@
 """ Faça um programa que calcule o número médio de alunos por turma. 
 Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma. 
 As turmas não podem ter mais de 40 alunos."""
-quantidadeturmas = int(input("Informe a quantidade de turmas:\n"))
-somadealunosporturma = 0
-for turma in range(1, quantidadeturmas + 1):
-    quantidadealunosporturma = int(
+quantidade_de_turmas = int(input("Informe a quantidade de turmas:\n"))
+soma_de_alunos_por_turma = 0
+for turma in range(1, quantidade_de_turmas + 1):
+    quantidade_alunos_por_turma = int(
         input(f"Informe a quantidade de alunos no {turma}° turma\n")
     )
-    if quantidadealunosporturma <= 40:
-        somadealunosporturma += quantidadealunosporturma
+    if quantidade_alunos_por_turma <= 40:
+        soma_de_alunos_por_turma += quantidade_alunos_por_turma
     else:
         print("O número de alunos passou de 40\n")
-        quantidadealunosporturma = int(
+        quantidade_alunos_por_turma = int(
             input(f"Informe a quantidade de alunos no {turma}° turma\n")
         )
 
-mediadealunosporturma = somadealunosporturma / quantidadeturmas
-print(f"A média de alunos por turma é {mediadealunosporturma}")
+media_de_alunos_por_turma = soma_de_alunos_por_turma / quantidade_de_turmas
+print(f"A média de alunos por turma é {media_de_alunos_por_turma}")
