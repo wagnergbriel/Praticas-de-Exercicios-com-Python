28ffa8f05de934e62029238d3ca9912e19b39de8
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:42:50 -0300

    refactor: reajustar nome das variaveis do exc_repeticao24

--- ListaEstruturadeRepetição/exc_repetição24.py
+++ ListaEstruturadeRepetição/exc_repetição24.py
@@ -1,10 +1,10 @@
 """ Faça um programa que calcule o mostre a média aritmética de N notas."""
-somadasnotas = 0
-quantidadenotas = int(input("Digite a quantidade de notas a ser calculada:\n"))
+soma_das_notas = 0
+quantidade_de_notas = int(input("Digite a quantidade de notas a ser calculada:\n"))
 
-for i in range(1, quantidadenotas + 1):
+for i in range(1, quantidade_de_notas + 1):
     notas = int(input(f"Digite a {i}° nota:\n"))
-    somadasnotas += notas
+    soma_das_notas += notas
 
-mediaaritimetica = somadasnotas / quantidadenotas
-print(f"\nA média aritmética é {mediaaritimetica:0.1f}")
+media_aritimetica = soma_das_notas / quantidade_de_notas
+print(f"\nA média aritmética é {media_aritimetica:0.1f}")
