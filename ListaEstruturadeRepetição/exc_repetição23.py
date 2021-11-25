1fd6500cf5dce9d0d81e9fe67003b03f8a942588
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:43:49 -0300

    refactor: reajustar nome da variavel do exc_repeticao23

--- ListaEstruturadeRepetição/exc_repetição23.py
+++ ListaEstruturadeRepetição/exc_repetição23.py
@@ -1,8 +1,8 @@
 """ Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário.
 O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos.
 Serão avaliados o funcionamento, o estilo e o número de testes (divisões) executados."""
-numeroprimo = int(input("Digite o número para verificar se é primo ou não:\n"))
+numero_primo = int(input("Digite o número para verificar se é primo ou não:\n"))
 
-for i in range(1, numeroprimo):
+for i in range(1, numero_primo):
     if i % 2 == 1 and i != 2:
         print(f"{i} é um número primo", end=" | ")
