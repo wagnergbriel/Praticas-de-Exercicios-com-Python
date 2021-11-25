98cdc60b525263f3290e13048b3c60f241e3ec2e
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:46:07 -0300

    refactor: reajustar nome da variavel do exc_repeticao21

--- ListaEstruturadeRepetição/exc_repetição21.py
+++ ListaEstruturadeRepetição/exc_repetição21.py
@@ -1,13 +1,13 @@
 """ Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. 
 Um número primo é aquele que é divisível somente por ele mesmo e por 1."""
-numeroprimo = int(input("Digite o número para verificar se é primo ou não:\n"))
+numero_primo = int(input("Digite o número para verificar se é primo ou não:\n"))
 divisor = 0
 
-for i in range(1, numeroprimo + 1):
-    if numeroprimo % i == 0:
+for i in range(1, numero_primo + 1):
+    if numero_primo % i == 0:
         divisor += 1
 
 if divisor == 2:
-    print(f"{numeroprimo} é um número primo")
+    print(f"{numero_primo} é um número primo")
 else:
-    print(f"{numeroprimo} não é um número primo")
+    print(f"{numero_primo} não é um número primo")
