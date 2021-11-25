481b852feb61b696a1a0ff15d41a30ab45a7920b
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-25 19:44:23 -0300

    refactor: reajustar nome da variavel do exc_repeticao22

--- ListaEstruturadeRepetição/exc_repetição22.py
+++ ListaEstruturadeRepetição/exc_repetição22.py
@@ -1,13 +1,13 @@
 ''' Altere o programa de cálculo dos números primos, 
 informando, caso o número não seja primo, por quais número ele é divisível.'''
-numeroprimo = int(input("Digite o número para verificar se é primo ou não:\n"))
+numero_primo = int(input("Digite o número para verificar se é primo ou não:\n"))
 divisor = 0
 
-for i in range(1, numeroprimo + 1):
-    if numeroprimo % i == 0:
+for i in range(1, numero_primo + 1):
+    if numero_primo % i == 0:
         divisor += 1
 
 if divisor == 2:
-    print(f"{numeroprimo} é um número primo")
+    print(f"{numero_primo} é um número primo")
 else:
-    print(f"{numeroprimo} não é um número primo, é divisível por {divisor} números")
+    print(f"{numero_primo} não é um número primo, é divisível por {divisor} números")
