ecdff5f00b7c2dca8763abbcbf2186efe737041e
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-26 18:24:24 -0300

    feat: adicionar a conclusao do exc_repeticao29

--- ListaEstruturadeRepetição/exc_repetição29.py
+++ ListaEstruturadeRepetição/exc_repetição29.py
@@ -2,3 +2,15 @@
 cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. 
 Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. 
 Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos"""
+lista_de_preços = []
+numero_da_tabela = 0
+
+for i in range(1, 5 + 1):
+    preço = float(input(f"Digite o {i}° preço da tabela:\n"))
+    lista_de_preços.append(preço)
+
+print("\nTabela de preço\n")
+
+for preço_na_tabela in lista_de_preços:
+    numero_da_tabela += 1
+    print(f"{numero_da_tabela} - R$ {preço_na_tabela:0.2f}\n")
